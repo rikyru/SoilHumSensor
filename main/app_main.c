9eface86dfc41811d154bea14ff9ad435aa6842b
ruggi <ruggiu92@gmail.com>
2025-05-28 23:03:59 +0200

    gestione disconnessioni

--- main/app_main.c
+++ main/app_main.c
@@ -5,14 +5,18 @@
 #include "nvs_flash.h"
 #include "esp_event.h"
 #include "esp_netif.h"
-
 #include "wifi_provisioning.h"
 #include "mqtt_wrapper.h"
 #include "sensor.h"
 #include "sleep_control.h"
 #include "config.h"
+#include "esp_wifi.h"
+#include "freertos/event_groups.h"
 
 static const char *TAG = "MAIN";
+static EventGroupHandle_t wifi_event_group;
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_DISCONNECTED_BIT BIT1
 
 void battery_task(void *param) {
     while (1) {
@@ -30,6 +34,25 @@ void battery_task(void *param) {
 }
 
 
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        ESP_LOGW(TAG, "Wi-Fi disconnected, trying to reconnect...");
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
+        xEventGroupSetBits(wifi_event_group, WIFI_DISCONNECTED_BIT);
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ESP_LOGI(TAG, "Got IP, starting MQTT...");
+
+        start_mqtt();
+        xTaskCreate(battery_task, "battery_task", 2048, NULL, 5, NULL);
+
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+        xEventGroupClearBits(wifi_event_group, WIFI_DISCONNECTED_BIT);
+    }
+}
+/*
 static void on_wifi_connected(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
     if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) 
@@ -47,6 +70,7 @@ static void on_wifi_connected(void* arg, esp_event_base_t event_base, int32_t ev
     //     sleep_control_enter();
     }
 }
+*/
 
 void app_main(void) {
     esp_err_t ret = nvs_flash_init();
@@ -67,7 +91,9 @@ void app_main(void) {
 
     // Event loop + handler per connessione Wi-Fi
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_wifi_connected, NULL));
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL));
 
     wifi_connect_from_config();  // si connette, poi chiama la callback
 }
